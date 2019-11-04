717aa9362185bf19de318d4573ee405d68eac685
Glen Hunter <hunter.glen@gmail.com>
2019-11-04 14:12:17 +0000

    PureRockets 1.23 Unity 2019.2.21f1 initial commit.

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.Analytics;
+
+public class AdManager : MonoBehaviour
+{
+    public void OnClick_ShowAd()
+    {
+        if(Advertisement.IsReady())
+        {
+            Debug.Log("Advertisement is ready");
+            Debug.Log("Attemping to show default ad");
+            Advertisement.Show();
+        }
+        else
+        {
+            Debug.Log("Advert is not ready");
+        }
+    }
+
+
+    public void OnClick_ShowPrivacyPolicy()
+    {
+        Application.OpenURL("https://unity3d.com/legal/privacy-policy");
+
+
+        int level = 1;
+        AnalyticsEvent.LevelComplete(level);
+    }
+}
---
+++ Assets/Scripts/AnalyticManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+
+
+public class AnalyticManager : MonoBehaviour
+{
+   public void OnClick_CompleteLevel(int level)
+   {
+       Debug.Log("Completed level: " + level);
+
+       AnalyticsResult result;
+       result = AnalyticsEvent.LevelComplete(level);
+       
+       Debug.Log("Analytics response/result: " + result.ToString());
+   }
+}
